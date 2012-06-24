7cd4558ae1cc1e4ab9727c754e9f5928f3ac101e
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-06-24 21:53:10 +0300

    rate added for registry.

--- abita/development/tests/test_setup.py
+++ abita/development/tests/test_setup.py
@@ -17,6 +17,29 @@ class TestCase(IntegrationTestCase):
         from plone.browserlayer import utils
         self.failUnless(IAbitaDevelopmentLayer in utils.registered_layers())
 
+    def get_record_rate(self):
+        from zope.component import getUtility
+        from plone.registry.interfaces import IRegistry
+        registry = getUtility(IRegistry)
+        return registry.records.get('abita.development.rate')
+
+    def test_registry_record__rate__field__instance(self):
+        record = self.get_record_rate()
+        from plone.registry.field import Float
+        self.assertIsInstance(record.field, Float)
+
+    def test_registry_record__rate__field__title(self):
+        record = self.get_record_rate()
+        self.assertEqual(record.field.title, u'Default Rate')
+
+    def test_registry_record__rate__field__description(self):
+        record = self.get_record_rate()
+        self.assertEqual(record.field.description, u'Default rate per 10 minutes.')
+
+    def test_registry_record__rate__value(self):
+        record = self.get_record_rate()
+        self.assertEqual(record.value, 6.0)
+
     def test_uninstall__package(self):
         installer = getToolByName(self.portal, 'portal_quickinstaller')
         installer.uninstallProducts(['abita.development'])
@@ -28,3 +51,14 @@ class TestCase(IntegrationTestCase):
         from abita.development.browser.interfaces import IAbitaDevelopmentLayer
         from plone.browserlayer import utils
         self.failIf(IAbitaDevelopmentLayer in utils.registered_layers())
+
+    def test_uninstall__registry_rate(self):
+        installer = getToolByName(self.portal, 'portal_quickinstaller')
+        installer.uninstallProducts(['abita.development'])
+        from zope.component import getUtility
+        from plone.registry.interfaces import IRegistry
+        registry = getUtility(IRegistry)
+        self.assertRaises(
+            KeyError,
+            lambda: registry['abita.development.rate']
+        )
