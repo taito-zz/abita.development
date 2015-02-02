e34859870fdcff2b8464958bc1db005dc1aedf1c
Taito Horiuchi <taito.horiuchi@gmail.com>
2015-02-02 16:31:18 +0200

    release 0.5.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='abita.development',
-    version='0.5',
+    version='0.5.1',
     description="",
     long_description=open("README.rst").read(),
     classifiers=[
